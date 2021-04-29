811cf26fd2dd53d8df7e45197ab6b0bc668fdef4
Carlos <cgmilan@gmail.com>
2021-04-28 20:03:02 -0500

    Add a Moving Ground Plane

--- src/App.js
+++ src/App.js
@@ -66,15 +66,45 @@ const ControlCamera = () => {
   );
 };
 
+const GROUND_HEIGHT = -50;
+
+const Floor = () => {
+  const floor = useRef();
+
+  useFrame(() => {
+    floor.current.position.z += 0.4;
+  });
+
+  return (
+    <mesh
+      visible
+      position={[0, GROUND_HEIGHT, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      ref={floor}
+    >
+      <planeBufferGeometry attach="geometry" args={[5000, 5000, 12, 12]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        roughness={1}
+        metalness={0}
+        wireframe
+      />
+    </mesh>
+  );
+};
+
 export default function App() {
   return (
     <>
       <Canvas style={{ background: "white" }}>
         <ControlCamera />
-        <directionalLight intensity={0.5} />
+        <directionalLight intensity={1} />
+        <ambientLight intensity={0.1} />
         <Suspense fallback={<Loading />}>
           <ArWing />
         </Suspense>
+        <Floor />
       </Canvas>
     </>
   );
